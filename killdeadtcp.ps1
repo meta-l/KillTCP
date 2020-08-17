419a86fd2c347e06e188441cc911c8f56866a7bb
meta-l <shrozzle@gmail.com>
2020-08-17 23:31:41 +0100

    Update killdeadtcp.ps1

--- killdeadtcp.ps1
+++ killdeadtcp.ps1
@@ -3,4 +3,10 @@ $pidnumRegex = [regex]"(\d+)$"
 $pidnum = $pidnumRegex.Match($netstatPID);
 taskkill /pid $pidnum.captures[0].Value /f
 
-#replace 1443 with your port of choice
+# replace 1443 with your port of choice
+
+# To find the port for an executable:
+$Get-Process | Select-Object -Property Name,Id,Description | Format-Table | findstr <exe_name>
+# id will be the PID
+$netstat -ao | findstr <pid>
+# wait
